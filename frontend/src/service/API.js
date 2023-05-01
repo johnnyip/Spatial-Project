05b17050dba2e3eab98700ecb43b666b81b1f643
unclechuen <johnny@johnnyip.com>
2023-05-02 01:20:33 +0800

    update for Docker

--- frontend/src/service/API.js
+++ frontend/src/service/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://spatial-back.johnnyip.com",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  baseURL: REACT_APP_BACKEND_URL !== undefined ? REACT_APP_BACKEND_URL : "https://spatial-back.johnnyip.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default instance;
